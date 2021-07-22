7abf2d2792ea9bc006d7c8cd2cc7a49744b05c5f
Adam Koprowski <adamkoprowski4@gmail.com>
2021-07-22 21:40:50 +0200

    add movie_details.html and change tmbd_client.py and main.py

--- movies_catalogue/main.py
+++ movies_catalogue/main.py
@@ -9,6 +9,14 @@ def homepage():
     movies = tmdb_client.get_movies(how_many=8)
     return render_template("homepage.html", movies=movies)
 
+
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+    details = tmdb_client.get_single_movie(movie_id)
+    cast = tmdb_client.get_single_movie_cast(movie_id)
+    return render_template("movie_details.html", movie=details, cast=cast)
+
+
 @app.context_processor
 def utility_processor():
     def tmdb_image_url(path, size):
--- movies_catalogue/tmdb_client.py
+++ movies_catalogue/tmdb_client.py
@@ -1,18 +1,40 @@
 import requests
 
+
+api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZDQxNDk5YjI4NmQyNjZjMmJiZjdlZmFmODZhZDA1OSIsInN1YiI6IjYwZjQ1MWU2ZjI4ODM4MDA1ZWRlY2RjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FhVtH1TkK-Wb4pKg6MbCyHCnp0PjkLQOAzKBBc8HDA4"
+
 def get_popular_movies():
     endpoint = "https://api.themoviedb.org/3/movie/popular"
-    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZDQxNDk5YjI4NmQyNjZjMmJiZjdlZmFmODZhZDA1OSIsInN1YiI6IjYwZjQ1MWU2ZjI4ODM4MDA1ZWRlY2RjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FhVtH1TkK-Wb4pKg6MbCyHCnp0PjkLQOAzKBBc8HDA4"
     headers = {
         "Authorization": f"Bearer {api_token}"
     }
     response = requests.get(endpoint, headers=headers)
     return response.json()
 
+
 def get_poster_url(poster_api_path, size="w342"):
     base_url = "https://image.tmdb.org/t/p/"
     return f"{base_url}{size}/{poster_api_path}"
 
+
 def get_movies(how_many):
     data = get_popular_movies()
     return data["results"][:how_many]
+
+
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+def get_single_movie_cast(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()["cast"]
+
